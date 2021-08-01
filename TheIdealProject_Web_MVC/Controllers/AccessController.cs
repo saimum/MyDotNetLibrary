5b83c3044a0b62b844abc1f03e02df57ca1ddbcc
saimum <saimum720@gmail.com>
2021-08-01 09:03:47 +0600

    First Commit

--- TheIdealProject_Web_MVC/Controllers/AccessController.cs
+++ TheIdealProject_Web_MVC/Controllers/AccessController.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TheIdealProject_Web_MVC.Models;
 
 namespace TheIdealProject_Web_MVC.Controllers
 {
     public class AccessController : Controller
     {
-        // GET: Access
+        DB_TheIdealPrtojectEntities DB = new DB_TheIdealPrtojectEntities();
         public ActionResult Index()
+        {
+            return View(DB.Tm_User.Where(a => a.TmUserPk > 1).ToList());
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Int64 Id)
         {
             return View();
         }
