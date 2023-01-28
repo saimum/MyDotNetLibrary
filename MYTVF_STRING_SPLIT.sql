135333174e827221d115ccd0c28bf86a4fc408e4
salauddin saimum <saimum720@gmail.com>
2023-01-28 15:00:30 +0600

    .

--- MYTVF_STRING_SPLIT.sql
+++ MYTVF_STRING_SPLIT.sql
@@ -1,28 +1,35 @@
-﻿Create FUNCTION [dbo].[MYTVF_STRING_SPLIT]
+﻿USE [master]
+GO
+/****** Object:  UserDefinedFunction [dbo].[MYTVF_STRING_SPLIT]    Script Date: 1/28/2023 11:11:39 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+Create FUNCTION [dbo].[MYTVF_STRING_SPLIT]
 (
-    @string NVARCHAR(MAX),
-    @delimiter CHAR(1),
-	@index int
+    @string nvarchar(MAX),
+    @delimiter nvarchar(1)
 )
 RETURNS @output TABLE(
-    RowIndex int,
-	RowData NVARCHAR(MAX)
+    Serial  int,
+	Item NVARCHAR(MAX)
 )
 BEGIN
-    DECLARE @start INT, @end INT, @_index INT = 0
+    DECLARE @start INT, @end INT, @_index INT = 1
     SELECT @start = 1, @end = CHARINDEX(@delimiter, @string)
 
     WHILE @start < LEN(@string) + 1 BEGIN
         IF @end = 0 
             SET @end = LEN(@string) + 1
-		IF(@_index = @index)
-			INSERT INTO @output (RowIndex,RowData) 
-			VALUES(@_index,SUBSTRING(@string, @start, @end - @start))
+
+        INSERT INTO @output (Serial,Item) 
+		VALUES(@_index,SUBSTRING(@string, @start, @end - @start))
         SET @start = @end + 1
         SET @end = CHARINDEX(@delimiter, @string, @start)
 		SET @_index = @_index+1;
     END
     RETURN
 END
-GO
---SELECT * FROM [master].dbo.[MYTVF_STRING_SPLIT]('John,Jeremy,Jack',',',0) 
+
+--SELECT * FROM [master].dbo.[MYTVF_STRING_SPLIT]('John,Jeremy,Jack',',') 
