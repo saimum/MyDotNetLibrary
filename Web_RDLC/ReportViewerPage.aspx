f3d03e56b6f58c06250ee6750dd00d5cbff0f269
salauddin saimum <saimum720@gmail.com>
2023-02-12 19:35:58 +0600

    .

---
+++ Web_RDLC/ReportViewerPage.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ReportViewerPage.aspx.cs" Inherits="Web_RDLC.ReportViewerPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+<form id="formCustomerReport" runat="server">  
+    <div>  
+        <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>  
+<rsweb:ReportViewer ID="CustomerListReportViewer" runat="server" Width="100%"></rsweb:ReportViewer>  
+    </div>  
+</form>
+</body>
+</html>
---
+++ Web_RDLC/ReportViewerPage.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Web_RDLC.Models;
+
+namespace Web_RDLC
+{
+    public partial class ReportViewerPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                string searchText = string.Empty;
+
+                if (Request.QueryString["searchText"] != null)
+                {
+                    searchText = Request.QueryString["searchText"].ToString();
+                }
+
+                List<Customer> customers = new List<Customer>() {
+                new Customer(){Id=1, Name="Cusotmer 1" },
+                new Customer(){Id=2, Name="Cusotmer 2" },
+                new Customer(){Id=3, Name="Cusotmer 3" },
+                new Customer(){Id=4, Name="Cusotmer 4" },
+                new Customer(){Id=5, Name="Cusotmer 5" },
+                new Customer(){Id=6, Name="Cusotmer 6" },
+                new Customer(){Id=7, Name="Cusotmer 7" },
+                new Customer(){Id=8, Name="Cusotmer 8" },
+                new Customer(){Id=9, Name="Cusotmer 9" },
+                new Customer(){Id=10, Name="Cusotmer 10" },
+                };
+                CustomerListReportViewer.LocalReport.ReportPath = Server.MapPath("~/Reports/RDLC/Report1.rdlc");
+                CustomerListReportViewer.LocalReport.DataSources.Clear();
+                ReportDataSource rdc = new ReportDataSource("CustomerDataSet", customers);
+                CustomerListReportViewer.LocalReport.DataSources.Add(rdc);
+                CustomerListReportViewer.LocalReport.Refresh();
+                CustomerListReportViewer.DataBind();
+            }
+        }
+    }
+}
