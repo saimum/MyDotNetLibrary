563380795aa657521c406d307875ceec870b779a
salauddin saimum <saimum720@gmail.com>
2023-02-21 19:39:54 +0600

    JWT request added

--- Web_RDLC/ReportViewerPage.aspx.cs
+++ Web_RDLC/ReportViewerPage.aspx.cs
@@ -14,31 +14,31 @@ namespace Web_RDLC
         {
             if (!Page.IsPostBack)
             {
-                string searchText = string.Empty;
+                //string searchText = string.Empty;
 
-                if (Request.QueryString["searchText"] != null)
-                {
-                    searchText = Request.QueryString["searchText"].ToString();
-                }
+                //if (Request.QueryString["searchText"] != null)
+                //{
+                //    searchText = Request.QueryString["searchText"].ToString();
+                //}
 
-                List<Customer> customers = new List<Customer>() {
-                new Customer(){Id=1, Name="Cusotmer 1" },
-                new Customer(){Id=2, Name="Cusotmer 2" },
-                new Customer(){Id=3, Name="Cusotmer 3" },
-                new Customer(){Id=4, Name="Cusotmer 4" },
-                new Customer(){Id=5, Name="Cusotmer 5" },
-                new Customer(){Id=6, Name="Cusotmer 6" },
-                new Customer(){Id=7, Name="Cusotmer 7" },
-                new Customer(){Id=8, Name="Cusotmer 8" },
-                new Customer(){Id=9, Name="Cusotmer 9" },
-                new Customer(){Id=10, Name="Cusotmer 10" },
-                };
-                CustomerListReportViewer.LocalReport.ReportPath = Server.MapPath("~/Reports/RDLC/Report1.rdlc");
-                CustomerListReportViewer.LocalReport.DataSources.Clear();
-                ReportDataSource rdc = new ReportDataSource("CustomerDataSet", customers);
-                CustomerListReportViewer.LocalReport.DataSources.Add(rdc);
-                CustomerListReportViewer.LocalReport.Refresh();
-                CustomerListReportViewer.DataBind();
+                //List<Customer> customers = new List<Customer>() {
+                //new Customer(){Id=1, Name="Cusotmer 1" },
+                //new Customer(){Id=2, Name="Cusotmer 2" },
+                //new Customer(){Id=3, Name="Cusotmer 3" },
+                //new Customer(){Id=4, Name="Cusotmer 4" },
+                //new Customer(){Id=5, Name="Cusotmer 5" },
+                //new Customer(){Id=6, Name="Cusotmer 6" },
+                //new Customer(){Id=7, Name="Cusotmer 7" },
+                //new Customer(){Id=8, Name="Cusotmer 8" },
+                //new Customer(){Id=9, Name="Cusotmer 9" },
+                //new Customer(){Id=10, Name="Cusotmer 10" },
+                //};
+                //CustomerListReportViewer.LocalReport.ReportPath = Server.MapPath("~/Reports/RDLC/Report1.rdlc");
+                //CustomerListReportViewer.LocalReport.DataSources.Clear();
+                //ReportDataSource rdc = new ReportDataSource("CustomerDataSet", customers);
+                //CustomerListReportViewer.LocalReport.DataSources.Add(rdc);
+                //CustomerListReportViewer.LocalReport.Refresh();
+                //CustomerListReportViewer.DataBind();
             }
         }
     }
---
+++ core3.1_JWT/Controllers/LoginController.cs
@@ -0,0 +1,69 @@
+﻿using core3._1_JWT.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace core3._1_JWT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IConfiguration _config;
+        private List<UserModel> UserModelList = new List<UserModel>();
+        public LoginController(IConfiguration config)
+        {
+            _config = config;
+            UserModelList.Add(new UserModel()
+            {
+                UserId = 1,
+                UserName = "A",
+                Password = "A",
+                Email = "A",
+            });
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login([FromBody] UserLogin userLogin)
+        {
+            var userModel = UserModelList.Where(m => m.UserName == userLogin.UserName).FirstOrDefault();
+            if (userModel != null)
+            {
+                var token = Generate(userModel);
+                return Ok(token);
+            }
+            else
+            {
+                return NotFound("NotFound");
+            }
+        }
+
+        private string Generate(UserModel userModel)
+        {
+            var Key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credential = new SigningCredentials(Key, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[] {
+            new Claim(ClaimTypes.NameIdentifier,userModel.UserName),
+            new Claim(ClaimTypes.Email,userModel.Email),
+            };
+            var token = new JwtSecurityToken(
+                _config["Jwt:Issuer"],
+                _config["Jwt:Audience"],
+                claims,
+                expires: DateTime.Now.AddSeconds(10),
+                signingCredentials: credential
+                );
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
